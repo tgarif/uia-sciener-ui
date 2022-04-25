e50125b5865980681dc8475d9c1e292b591116a8
Tengku Arif <arereeftengku@gmail.com>
2022-04-25 12:53:37 +0800

    added electron

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,22 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
+import tsconfigPaths from 'vite-tsconfig-paths';
+const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: path.resolve(__dirname, './dist/'),
   plugins: [
     vue({
       template: { transformAssetUrls },
     }),
 
     quasar({
-      autoImportComponentCase: "pascal",
-      sassVariables: "src/assets/sass/quasar-variables.sass",
+      autoImportComponentCase: 'pascal',
+      sassVariables: 'src/assets/sass/quasar-variables.sass',
     }),
+
+    tsconfigPaths(),
   ],
 });
