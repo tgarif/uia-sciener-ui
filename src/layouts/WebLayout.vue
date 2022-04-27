ec934eda4a47832c6414ab754e0e7d0f588ff730
Tengku Arif <arereeftengku@gmail.com>
2022-04-27 19:35:32 +0800

    fix image path

--- src/layouts/WebLayout.vue
+++ src/layouts/WebLayout.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { applicationStore } from '../stores/application.store';
+import Logo from '../assets/images/logo.svg';
 
 const application = applicationStore();
 </script>
@@ -9,7 +10,7 @@ const application = applicationStore();
     <q-header elevated class="bg-dork text-white" height-hint="98">
       <q-toolbar>
         <q-toolbar-title class="header--title q-gutter-md">
-          <q-img src="/src/assets/images/Logo.svg" style="width: 3rem" />
+          <q-img :src="Logo" style="width: 3rem" />
           <div class="text-h4">UIA Sciener Integration</div>
         </q-toolbar-title>
       </q-toolbar>
